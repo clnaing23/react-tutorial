16fecffc8d3cba009bbd083e6798013c3ca33651
Chris Naing <clnaing23@gmail.com>
2022-09-26 23:31:21 -0500

    added cs courses 2018-2019

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
 const App = () => {
   const [count, setCount] = useState(0);
 
@@ -9,7 +11,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <h1>{schedule.title}!</h1>
         <p>
           <button onClick={() => setCount(count => count + 1)}>
             count is: {count}
